b121d6b62ada24d4879e7ebda3a85113fcd3eeb6
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-05-02 23:05:39 +0500

    feat: options added

---
+++ lua/user/options.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.g.mapleader = " "
