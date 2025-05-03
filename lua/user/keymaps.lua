0fa8833550ef699b19194b731cda687b4ca26b64
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-05-03 15:07:40 +0500

    refactor(nvimtree): nvimtree updated & keymaps added

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle File Explorer" })
+vim.keymap.set("n", "<leader>h", ":NvimTreeFocus<CR>", { noremap = true, silent = true, desc = "Focus File Explorer" })
+vim.keymap.set("n", "<leader>g", ":wincmd p<CR>", { noremap = true, silent = true, desc = "Focus Editor" })
--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -1,59 +1,32 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  vim.notify("nvim-tree not found!")
+  vim.notify("nvim-tree not found!", vim.log.levels.ERROR)
   return
 end
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-web-devicons").setup({})
+pcall(require, "nvim-web-devicons")
 
 nvim_tree.setup({
   auto_reload_on_write = true,
   disable_netrw = true,
   hijack_cursor = true,
   hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
-  sort_by = "name",
-  root_dirs = {},
-  prefer_startup_root = false,
   sync_root_with_cwd = true,
-  reload_on_bufenter = false,
-  respect_buf_cwd = false,
-  on_attach = "default",
-  select_prompts = false,
+
   view = {
-    centralize_selection = false,
-    cursorline = true,
-    debounce_delay = 15,
-    width = 30,
+    width = 40,
     side = "left",
-    preserve_window_proportions = false,
-    number = false,
-    relativenumber = false,
+    cursorline = true,
     signcolumn = "yes",
     float = {
       enable = false,
-      quit_on_focus_loss = true,
-      open_win_config = {
-        relative = "editor",
-        border = "rounded",
-        width = 30,
-        height = 30,
-        row = 1,
-        col = 1,
-      },
     },
   },
+
   renderer = {
-    add_trailing = false,
-    group_empty = false,
-    highlight_git = false,
-    full_name = false,
-    highlight_opened_files = "none",
-    highlight_modified = "none",
-    root_folder_label = ":~:s?$?/..?",
     indent_width = 2,
     indent_markers = {
       enable = true,
@@ -67,11 +40,6 @@ nvim_tree.setup({
       },
     },
     icons = {
-      webdev_colors = true,
-      git_placement = "before",
-      modified_placement = "after",
-      padding = " ",
-      symlink_arrow = " ➛ ",
       show = {
         file = true,
         folder = true,
@@ -80,75 +48,33 @@ nvim_tree.setup({
         modified = true,
       },
       glyphs = {
-        default = "",
-        symlink = "",
-        bookmark = "",
         modified = "●",
-        folder = {
-          arrow_closed = "",
-          arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
         git = {
           unstaged = "✗",
           staged = "✓",
-          unmerged = "",
           renamed = "➜",
           untracked = "★",
-          deleted = "",
           ignored = "◌",
         },
       },
     },
+    root_folder_label = ":~:s?$?/..?",
     special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
-    symlink_destination = true,
   },
+
   hijack_directories = {
     enable = true,
     auto_open = true,
   },
+
   update_focused_file = {
     enable = true,
-    update_root = false,
-    ignore_list = {},
   },
-  system_open = {
-    cmd = "",
-    args = {},
-  },
-  diagnostics = {
-    enable = false,
-    show_on_dirs = false,
-    show_on_open_dirs = true,
-    debounce_delay = 50,
-    severity = {
-      min = vim.diagnostic.severity.HINT,
-      max = vim.diagnostic.severity.ERROR,
-    },
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
+
   filters = {
     dotfiles = false,
-    git_clean = false,
-    no_buffer = false,
-    custom = {},
-    exclude = {},
   },
-  filesystem_watchers = {
-    enable = true,
-    debounce_delay = 50,
-    ignore_dirs = {},
-  },
+
   git = {
     enable = true,
     ignore = false,
@@ -156,32 +82,15 @@ nvim_tree.setup({
     show_on_open_dirs = true,
     timeout = 400,
   },
+
   actions = {
     use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
-    expand_all = {
-      max_folder_discovery = 300,
-      exclude = {},
-    },
-    file_popup = {
-      open_win_config = {
-        col = 1,
-        row = 1,
-        relative = "cursor",
-        border = "shadow",
-        style = "minimal",
-      },
-    },
+    change_dir = { enable = true },
     open_file = {
       quit_on_open = false,
       resize_window = true,
       window_picker = {
         enable = true,
-        picker = "default",
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
         exclude = {
           filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
@@ -193,40 +102,23 @@ nvim_tree.setup({
       close_window = true,
     },
   },
+
   trash = {
     cmd = "trash",
     require_confirm = true,
   },
+
   live_filter = {
     prefix = "[FILTER]: ",
     always_show_folders = true,
   },
-  tab = {
-    sync = {
-      open = false,
-      close = false,
-      ignore = {},
-    },
-  },
+
   notify = {
     threshold = vim.log.levels.INFO,
   },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      dev = false,
-      diagnostics = false,
-      git = false,
-      profile = false,
-      watcher = false,
-    },
-  },
 })
 
-vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+vim.keymap.set('n', '<leader>r', ':NvimTreeRefresh<CR>', opts)
+vim.keymap.set('n', '<leader>n', ':NvimTreeFindFile<CR>', opts)
