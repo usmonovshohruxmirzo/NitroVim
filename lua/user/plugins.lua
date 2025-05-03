698d77bce832e93b47b318dc60b9001a017636d0
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-05-03 10:56:04 +0500

    feat: plugins added

---
+++ lua/user/plugins.lua
@@ -0,0 +1,15 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim",
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "navarasu/onedark.nvim" },
+  { "folke/tokyonight.nvim" }
+})
