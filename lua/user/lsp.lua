93957174ec4c33362a6cb0849768341175ad64c2
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-08 18:46:24 +0500

    refactor: added more langs to lsp

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,22 +1,59 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-  ensure_installed = { "tsserver", "pyright", "gopls", "clangd" }, -- Add your preferred language servers
+  ensure_installed = {
+    "ts_ls",
+    "eslint",
+    "html",
+    "cssls",
+    "tailwindcss",
+    "emmet_ls",
+    "jsonls",
+    "prismals",
+    "volar",
+  },
 })
 
-local nvim_lsp = require("lspconfig")
 
-nvim_lsp.tsserver.setup({
-  on_attach = function(client, bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-  end,
-  flags = {
-    debounce_text_changes = 150,
-  }
+local lspconfig = require("lspconfig")
+
+local on_attach = function(client, bufnr)
+  local opts = { noremap=true, silent=true }
+  local map = vim.api.nvim_buf_set_keymap
+
+  map(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  map(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  map(bufnr, 'n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  map(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+end
+
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
 })
 
-nvim_lsp.pyright.setup({
-  on_attach = function(client, bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-  end,
+lspconfig.eslint.setup({
+  on_attach = on_attach,
+})
+
+lspconfig.html.setup({
+  on_attach = on_attach,
+})
+
+lspconfig.cssls.setup({
+  on_attach = on_attach,
+})
+
+lspconfig.tailwindcss.setup({
+  on_attach = on_attach,
+})
+
+lspconfig.emmet_ls.setup({
+  filetypes = {
+    "html", "css", "scss", "sass",
+    "javascript", "javascriptreact",
+    "typescript", "typescriptreact",
+  },
+})
+lspconfig.jsonls.setup({
+  on_attach = on_attach,
 })
