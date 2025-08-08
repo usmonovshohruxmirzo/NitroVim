8b39a53663ecffd5c8bc0c0a2fcd9e35d0a0922a
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-08 18:45:52 +0500

    refactor: added plugin autosave, css utils, treesitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -9,47 +9,100 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+
+  -- File Explorer
   { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+
+  -- Themes
   { "navarasu/onedark.nvim" },
   { "folke/tokyonight.nvim" },
+
+  -- Bufferline
   {
     "akinsho/bufferline.nvim",
     version = "*",
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
+
+  -- Statusline
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-  },
-  {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require('user.dashboard')
+      require("lualine").setup()
     end,
   },
+
+  -- Dashboard
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("user.dashboard")
+    end,
+  },
+
+  -- Telescope
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
+
+  -- Terminal
   {
-  "neovim/nvim-lspconfig",
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim"
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true
   },
+
+  -- LSP + Mason
   {
-  "hrsh7th/nvim-cmp",             -- Autocompletion plugin
-  "hrsh7th/cmp-nvim-lsp",         -- LSP source for nvim-cmp
-  "hrsh7th/cmp-buffer",           -- Buffer completion
-  "hrsh7th/cmp-path",             -- Path completion
-  "hrsh7th/cmp-cmdline",          -- Command line completion
-  "saadparwaiz1/cmp_luasnip",     -- Snippets completion (optional)
-  "L3MON4D3/LuaSnip",             -- Snippet engine (optional)
-  "williamboman/mason.nvim",      -- Tool for managing LSP servers
-  "williamboman/mason-lspconfig.nvim", -- Integration with LSP config
-  "neovim/nvim-lspconfig"         -- LSP client setup
-},
-{ "rafamadriz/friendly-snippets" },
-{'akinsho/toggleterm.nvim', version = "*", config = true}
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+  },
+
+  -- Completion (nvim-cmp)
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "saadparwaiz1/cmp_luasnip" },
+
+  -- Snippets
+  { "L3MON4D3/LuaSnip" },
+  { "rafamadriz/friendly-snippets" },
+
+  -- Treesitter
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  
+  -- CSS utilities 
+  { "norcalli/nvim-colorizer.lua" },
+  { "NvChad/nvim-colorizer.lua" },       
+
+  -- Auto Save
+  {
+  "pocco81/auto-save.nvim",
+  config = function()
+    require("auto-save").setup {
+      enabled = true,
+      execution_message = {
+        message = function() return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S")) end,
+        dim = 0.18,
+        cleaning_interval = 1250,
+      },
+      trigger_events = {"InsertLeave", "TextChanged"},
+      conditions = {
+        exists = true,
+        filename_is_not = {},
+        filetype_is_not = {},
+        modifiable = true,
+      },
+      write_all_buffers = false,
+      debounce_delay = 135,
+    }
+    end,
+  }
 })
