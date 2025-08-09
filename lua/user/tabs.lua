7ed2341fa131614a4df887f0c455273c6740f706
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-09 11:21:10 +0500

    refactor(tabs): update bufferline update

--- lua/user/tabs.lua
+++ lua/user/tabs.lua
@@ -5,6 +5,15 @@ require("bufferline").setup({
     options = {
         mode = "buffers",
         separator_style = "slant",
+        offsets = {
+         {
+           filetype = "NvimTree",
+           text = "File Explorer",
+           text_align = "left",
+           separator = false,
+           padding = 0
+         },
+       },
     },
     highlights = {
         buffer_selected = {
