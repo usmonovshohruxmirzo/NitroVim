1e50adec9bfc7486f88e6955ccde943c807f124e
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-09 10:40:33 +0500

    refactor(tabs): update bufferline style

--- lua/user/tabs.lua
+++ lua/user/tabs.lua
@@ -1,3 +1,15 @@
-require("bufferline").setup()
 vim.opt.termguicolors = true
 vim.opt.showtabline = 5
+
+require("bufferline").setup({
+    options = {
+        mode = "buffers",
+        separator_style = "slant",
+    },
+    highlights = {
+        buffer_selected = {
+            bold = true,
+            italic = false,
+        }
+    }
+})
