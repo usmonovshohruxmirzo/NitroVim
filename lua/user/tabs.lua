c87d0063ceb58f117ca8501eacf7a2853cbf6de7
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-11 23:08:28 +0500

    refactor(tabs): add diagnosticsm update text align of nvim tree text

--- lua/user/tabs.lua
+++ lua/user/tabs.lua
@@ -5,11 +5,12 @@ require("bufferline").setup({
     options = {
         mode = "buffers",
         separator_style = "slant",
+        diagnostics = "nvim_lsp",
         offsets = {
          {
            filetype = "NvimTree",
            text = "File Explorer",
-           text_align = "left",
+           text_align = "center",
            separator = false,
            padding = 0
          },
@@ -19,6 +20,6 @@ require("bufferline").setup({
         buffer_selected = {
             bold = true,
             italic = false,
-        }
+        },
     }
 })
