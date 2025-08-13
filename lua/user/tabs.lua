96601bc8949c52c26bfe20850b9c7eac775a5cf7
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-13 22:35:04 +0500

    refactor: change tab style to 'thin'

--- lua/user/tabs.lua
+++ lua/user/tabs.lua
@@ -2,24 +2,21 @@ vim.opt.termguicolors = true
 vim.opt.showtabline = 5
 
 require("bufferline").setup({
-    options = {
-        mode = "buffers",
-        separator_style = "slant",
-        diagnostics = "nvim_lsp",
-        offsets = {
-         {
-           filetype = "NvimTree",
-           text = "File Explorer",
-           text_align = "center",
-           separator = false,
-           padding = 0
-         },
-       },
+  options = {
+    mode = "buffers",
+    separator_style = "thin",
+    diagnostics = "nvim_lsp",
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        text_align = "center",
+        separator = false,
+        padding = 0,
+      },
     },
-    highlights = {
-        buffer_selected = {
-            bold = true,
-            italic = false,
-        },
-    }
+  },
+  highlights = {
+    buffer_selected = { bold = true, italic = false },
+  },
 })
