5839935a7a2865e0fcc9d58b1553d7f828911de0
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-19 17:59:06 +0500

    feat: add commands.lua
    - add MkDir command for creating folder

---
+++ lua/user/commands.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_user_command(
+  "MkDir",
+  function(opts)
+    local dir = opts.args
+    if dir == "" then
+      print("Please provide a folder path!")
+      return
+    end
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+      print("Folder created: " .. dir)
+    else
+      print("Folder already exists: " .. dir)
+    end
+  end,
+  { nargs = 1 }
+)
