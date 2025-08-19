5176a1e0954f51dfb1933f269305d4325dc53bed
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-19 12:21:19 +0500

    fix(telescope): add missing comma in file_ignore_patterns to properly ignore `.git` & `node_modules` & `.next`

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,3 +1,13 @@
+require('telescope').setup{
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules",
+      ".git",
+      ".next"
+    },
+  },
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
