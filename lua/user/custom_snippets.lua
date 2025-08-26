abe330cd025db40fe903f5b69ed579c0efcd4faa
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-26 23:15:02 +0500

    chore(custom_snippets): format code

--- lua/user/custom_snippets.lua
+++ lua/user/custom_snippets.lua
@@ -8,202 +8,202 @@ local rep = require("luasnip.extras").rep
 -- HTML
 ls.add_snippets("html", {
   s("base", {
-      t({"<!DOCTYPE html>", "<html lang=\"en\">", "<head>", "    <meta charset=\"UTF-8\">", "    <title>"}), 
-      i(1, "Title"), 
-      t({"</title>", "</head>", "<body>", ""}), 
-      i(2, "Content"), 
-      t({"", "</body>", "</html>"}), 
+    t({ "<!DOCTYPE html>", "<html lang=\"en\">", "<head>", "    <meta charset=\"UTF-8\">", "    <title>" }),
+    i(1, "Title"),
+    t({ "</title>", "</head>", "<body>", "" }),
+    i(2, "Content"),
+    t({ "", "</body>", "</html>" }),
   }),
 
   -- Extends
   s("extends", {
-      t("{% extends '"), i(1, "base.html"), t("' %}"),
+    t("{% extends '"), i(1, "base.html"), t("' %}"),
   }),
 
   -- Block
   s("block", {
-      t("{% block "), i(1, "block_name"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endblock "}), i(3, "block_name"), t(" %}"),
+    t("{% block "), i(1, "block_name"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endblock " }), i(3, "block_name"), t(" %}"),
   }),
 
   -- URL tag
   s("url", {
-      t("{% url '"), i(1, "view_name"), t("' %}"),
+    t("{% url '"), i(1, "view_name"), t("' %}"),
   }),
 
   -- Static files
   s("static", {
-      t("{% static '"), i(1, "path/to/file"), t("' %}"),
+    t("{% static '"), i(1, "path/to/file"), t("' %}"),
   }),
 
   -- Load static
   s("loadstatic", {
-      t("{% load static %}"),
+    t("{% load static %}"),
   }),
 
   -- If condition
   s("if", {
-      t("{% if "), i(1, "condition"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endif %}"}),
+    t("{% if "), i(1, "condition"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endif %}" }),
   }),
 
   -- If-Else
   s("ifelse", {
-      t("{% if "), i(1, "condition"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content if true"), 
-      t({"", "{% else %}", ""}), 
-      i(3, "Content if false"), 
-      t({"", "{% endif %}"}),
+    t("{% if "), i(1, "condition"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content if true"),
+    t({ "", "{% else %}", "" }),
+    i(3, "Content if false"),
+    t({ "", "{% endif %}" }),
   }),
 
   -- For loop
   s("for", {
-      t("{% for "), i(1, "item in list"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endfor %}"}),
+    t("{% for "), i(1, "item in list"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endfor %}" }),
   }),
 
   -- Comment
   s("comment", {
-      t("{# "), i(1, "Comment"), t(" #}"),
+    t("{# "), i(1, "Comment"), t(" #}"),
   }),
 
   -- Include template
   s("include", {
-      t("{% include '"), i(1, "template.html"), t("' %}"),
+    t("{% include '"), i(1, "template.html"), t("' %}"),
   }),
 
   -- With statement
   s("with", {
-      t("{% with "), i(1, "var=value"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endwith %}"}),
+    t("{% with "), i(1, "var=value"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endwith %}" }),
   }),
 
   -- Autoescape
   s("autoescape", {
-      t("{% autoescape "), i(1, "on/off"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endautoescape %}"}),
+    t("{% autoescape "), i(1, "on/off"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endautoescape %}" }),
   }),
 
   -- csrf token
   s("csrf", {
-      t("{% csrf_token %}"),
+    t("{% csrf_token %}"),
   }),
 
   -- Filter example
   s("filter", {
-      t("{{ "), i(1, "variable"), t(" | "), i(2, "filter_name"), t(" }}"),
+    t("{{ "), i(1, "variable"), t(" | "), i(2, "filter_name"), t(" }}"),
   }),
 })
 
 -- Django
 ls.add_snippets("djangohtml", {
   s("base", {
-      t({"<!DOCTYPE html>", "<html lang=\"en\">", "<head>", "    <meta charset=\"UTF-8\">", "    <title>"}), 
-      i(1, "Title"), 
-      t({"</title>", "</head>", "<body>", ""}), 
-      i(2, "Content"), 
-      t({"", "</body>", "</html>"}), 
+    t({ "<!DOCTYPE html>", "<html lang=\"en\">", "<head>", "    <meta charset=\"UTF-8\">", "    <title>" }),
+    i(1, "Title"),
+    t({ "</title>", "</head>", "<body>", "" }),
+    i(2, "Content"),
+    t({ "", "</body>", "</html>" }),
   }),
 
   -- Extends
   s("extends", {
-      t("{% extends '"), i(1, "base.html"), t("' %}"),
+    t("{% extends '"), i(1, "base.html"), t("' %}"),
   }),
 
   -- Block
   s("block", {
-      t("{% block "), i(1, "block_name"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endblock "}), i(3, "block_name"), t(" %}"),
+    t("{% block "), i(1, "block_name"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endblock " }), i(3, "block_name"), t(" %}"),
   }),
 
   -- URL tag
   s("url", {
-      t("{% url '"), i(1, "view_name"), t("' %}"),
+    t("{% url '"), i(1, "view_name"), t("' %}"),
   }),
 
   -- Static files
   s("static", {
-      t("{% static '"), i(1, "path/to/file"), t("' %}"),
+    t("{% static '"), i(1, "path/to/file"), t("' %}"),
   }),
 
   -- Load static
   s("loadstatic", {
-      t("{% load static %}"),
+    t("{% load static %}"),
   }),
 
   -- If condition
   s("if", {
-      t("{% if "), i(1, "condition"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endif %}"}),
+    t("{% if "), i(1, "condition"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endif %}" }),
   }),
 
   -- If-Else
   s("ifelse", {
-      t("{% if "), i(1, "condition"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content if true"), 
-      t({"", "{% else %}", ""}), 
-      i(3, "Content if false"), 
-      t({"", "{% endif %}"}),
+    t("{% if "), i(1, "condition"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content if true"),
+    t({ "", "{% else %}", "" }),
+    i(3, "Content if false"),
+    t({ "", "{% endif %}" }),
   }),
 
   -- For loop
   s("for", {
-      t("{% for "), i(1, "item in list"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endfor %}"}),
+    t("{% for "), i(1, "item in list"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endfor %}" }),
   }),
 
   -- Comment
   s("comment", {
-      t("{# "), i(1, "Comment"), t(" #}"),
+    t("{# "), i(1, "Comment"), t(" #}"),
   }),
 
   -- Include template
   s("include", {
-      t("{% include '"), i(1, "template.html"), t("' %}"),
+    t("{% include '"), i(1, "template.html"), t("' %}"),
   }),
 
   -- With statement
   s("with", {
-      t("{% with "), i(1, "var=value"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endwith %}"}),
+    t("{% with "), i(1, "var=value"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endwith %}" }),
   }),
 
   -- Autoescape
   s("autoescape", {
-      t("{% autoescape "), i(1, "on/off"), t(" %}"), 
-      t({"", ""}), 
-      i(2, "Content"), 
-      t({"", "{% endautoescape %}"}),
+    t("{% autoescape "), i(1, "on/off"), t(" %}"),
+    t({ "", "" }),
+    i(2, "Content"),
+    t({ "", "{% endautoescape %}" }),
   }),
 
   -- csrf token
   s("csrf", {
-      t("{% csrf_token %}"),
+    t("{% csrf_token %}"),
   }),
 
   -- Filter example
   s("filter", {
-      t("{{ "), i(1, "variable"), t(" | "), i(2, "filter_name"), t(" }}"),
+    t("{{ "), i(1, "variable"), t(" | "), i(2, "filter_name"), t(" }}"),
   }),
 })
 
@@ -307,7 +307,7 @@ ls.add_snippets("cs", {
 
   -- Class
   s("class", fmt([[
-    public class {} 
+    public class {}
     {{
         {}
     }}
@@ -322,8 +322,8 @@ ls.add_snippets("cs", {
   ]], { i(1, "Console.WriteLine(\"Hello World\");") })),
 
   -- For loop
-s("for", fmt([[
-  for (int {} = 0; {} < {}; {}++) 
+  s("for", fmt([[
+  for (int {} = 0; {} < {}; {}++)
   {{
       {}
   }}
@@ -331,7 +331,7 @@ s("for", fmt([[
 
   -- If statement
   s("if", fmt([[
-    if ({}) 
+    if ({})
     {{
         {}
     }}
@@ -343,5 +343,5 @@ s("for", fmt([[
   ]], { i(1, "string"), i(2, "PropertyName") })),
 
   -- get/set
-s("get", t("{ get; set; }")),
+  s("get", t("{ get; set; }")),
 })
