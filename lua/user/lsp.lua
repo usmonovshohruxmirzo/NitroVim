755840a5fc1fd94a4bba4609c4e9bb94841b63e6
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-30 18:29:19 +0500

    chore(lsp): format code

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,35 +1,7 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "ts_ls",
-        "eslint",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "emmet_language_server",
-        "jsonls",
-        "pyright",
-        "omnisharp",
-    },
-    automatic_installation = true,
-})
-
-local on_attach = function(client, bufnr)
-    local opts = { noremap = true, silent = true }
-    local map = vim.api.nvim_buf_set_keymap
-
-    map(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    map(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    map(bufnr, "n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    map(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    map(bufnr, "n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    map(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
-end
-
-local lspconfig = require("lspconfig")
-
-local servers = {
+  ensure_installed = {
     "ts_ls",
     "eslint",
     "html",
@@ -39,19 +11,47 @@ local servers = {
     "jsonls",
     "pyright",
     "omnisharp",
+  },
+  automatic_installation = true,
+})
+
+local on_attach = function(client, bufnr)
+  local opts = { noremap = true, silent = true }
+  local map = vim.api.nvim_buf_set_keymap
+
+  map(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  map(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  map(bufnr, "n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  map(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  map(bufnr, "n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  map(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
+end
+
+local lspconfig = require("lspconfig")
+
+local servers = {
+  "ts_ls",
+  "eslint",
+  "html",
+  "cssls",
+  "tailwindcss",
+  "emmet_language_server",
+  "jsonls",
+  "pyright",
+  "omnisharp",
 }
 
 for _, server in ipairs(servers) do
-    lspconfig[server].setup({
-        on_attach = on_attach,
-    })
+  lspconfig[server].setup({
+    on_attach = on_attach,
+  })
 end
 
 lspconfig.omnisharp.setup({
-    cmd = { "omnisharp" },
-    on_attach = on_attach,
-    enable_editorconfig_support = true,
-    enable_roslyn_analyzers = true,
-    organize_imports_on_format = true,
-    enable_import_completion = true,
+  cmd = { "omnisharp" },
+  on_attach = on_attach,
+  enable_editorconfig_support = true,
+  enable_roslyn_analyzers = true,
+  organize_imports_on_format = true,
+  enable_import_completion = true,
 })
