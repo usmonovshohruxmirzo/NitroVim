3e4c456448b3b8c6665f8c6771f9151ab2122f48
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-09-22 17:48:55 +0500

    fix(lsp): The require('lspconfig') "framework" is deprecated

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -15,17 +15,19 @@ require("mason-lspconfig").setup({
   automatic_installation = true,
 })
 
-local on_attach = function(client, bufnr)
-  local opts = { noremap = true, silent = true }
-  local map = vim.api.nvim_buf_set_keymap
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local opts = { noremap = true, silent = true, buffer = bufnr }
 
-  map(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  map(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  map(bufnr, "n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  map(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  map(bufnr, "n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  map(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
-end
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  end,
+})
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 local cmp_ok, cmp_lsp = pcall(require, "cmp_nvim_lsp")
@@ -33,9 +35,22 @@ if cmp_ok then
   capabilities = cmp_lsp.default_capabilities(capabilities)
 end
 
-local lspconfig = require("lspconfig")
+vim.lsp.config('*', {
+  capabilities = capabilities,
+})
 
-local servers = {
+vim.lsp.config('omnisharp', {
+  cmd = { "omnisharp" },
+  enable_editorconfig_support = true,
+  enable_roslyn_analyzers = true,
+  organize_imports_on_format = true,
+  enable_import_completion = true,
+  handlers = {
+    ["textDocument/definition"] = require('omnisharp_extended').handler,
+  },
+})
+
+vim.lsp.enable({
   "ts_ls",
   "eslint",
   "html",
@@ -44,24 +59,5 @@ local servers = {
   "emmet_language_server",
   "jsonls",
   "pyright",
-}
-
-for _, server in ipairs(servers) do
-  lspconfig[server].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
-end
-
-lspconfig.omnisharp.setup({
-  cmd = { "omnisharp" },
-  on_attach = on_attach,
-  capabilities = capabilities,
-  enable_editorconfig_support = true,
-  enable_roslyn_analyzers = true,
-  organize_imports_on_format = true,
-  enable_import_completion = true,
-  handlers = {
-    ["textDocument/definition"] = require('omnisharp_extended').handler,
-  },
+  "omnisharp",
 })
