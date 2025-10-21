c48cfb4f5034fbfff7e251575c70bd5e207ed1c6
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-10-21 22:45:35 +0500

    refactor(options): add Search, Performance, File handling options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -2,7 +2,7 @@
 ---@type table
 
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
@@ -12,6 +12,25 @@ vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = " "
 vim.opt.fillchars:append({ eob = " " })
 vim.o.autoread = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+-- Performance
+vim.opt.lazyredraw = true
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+
+-- File Handling
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.hidden = true
+vim.opt.confirm = true
 
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
   command = "checktime",
