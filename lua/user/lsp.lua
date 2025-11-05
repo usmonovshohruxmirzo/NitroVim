630e3fcf31ed750eb404e950de22bfa5c6afc58e
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2025-11-05 11:45:22 +0500

    fix(lsp): enhenced omnisharp lsp and cleanup overall structure

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -15,7 +15,7 @@ require("mason-lspconfig").setup({
     "pyright",
     "omnisharp",
     "lua_ls",
-    "rust_analyzer"
+    "rust_analyzer",
   },
   automatic_installation = true,
 })
@@ -24,13 +24,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local bufnr = args.buf
     local opts = { noremap = true, silent = true, buffer = bufnr }
+    local buf = vim.lsp.buf
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gd", buf.definition, opts)
+    vim.keymap.set("n", "K", buf.hover, opts)
+    vim.keymap.set("n", "gi", buf.implementation, opts)
+    vim.keymap.set("n", "<leader>rn", buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", buf.code_action, opts)
+    vim.keymap.set("n", "gr", buf.references, opts)
+    vim.keymap.set("n", "<leader>f", function() buf.format({ async = true }) end, opts)
   end,
 })
 
@@ -40,27 +42,14 @@ if cmp_ok then
   capabilities = cmp_lsp.default_capabilities(capabilities)
 end
 
-vim.lsp.config('*', {
-  capabilities = capabilities,
-})
+vim.lsp.config("*", { capabilities = capabilities })
 
-vim.lsp.config('omnisharp', {
-  cmd = { "omnisharp" },
-  enable_editorconfig_support = true,
-  enable_roslyn_analyzers = true,
-  organize_imports_on_format = true,
-  enable_import_completion = true,
-  handlers = {
-    ["textDocument/definition"] = require('omnisharp_extended').handler,
-  },
-  capabilities = capabilities,
-})
-
-local function get_server_cmd(server_name)
-  local mappings = require('mason-lspconfig').get_mappings()
-  local cmd_path = mappings[server_name]
-  if cmd_path then
-    return { cmd_path }
+local function mason_bin(name)
+  local bin = vim.fn.stdpath("data") .. "/mason/bin/" .. name
+  if vim.fn.executable(bin) == 1 then
+    return bin
+  elseif vim.fn.executable(name) == 1 then
+    return name
   else
     return nil
   end
@@ -71,10 +60,11 @@ local function start_lsp(server_name, filetypes, extra_config)
   vim.api.nvim_create_autocmd("FileType", {
     pattern = filetypes,
     callback = function()
-      local config = vim.tbl_extend('force', {
+      local root = vim.fs.dirname(vim.fs.find({ ".git", "package.json" }, { upward = true })[1])
+      local config = vim.tbl_extend("force", {
         name = server_name,
-        cmd = get_server_cmd(server_name),
-        root_dir = vim.fs.dirname(vim.fs.find({ ".git", "package.json" }, { upward = true })[1]),
+        cmd = mason_bin(server_name) and { mason_bin(server_name) } or nil,
+        root_dir = root,
         capabilities = capabilities,
       }, extra_config)
       vim.lsp.start(config)
@@ -90,14 +80,72 @@ start_lsp("tailwindcss", { "html", "css", "typescriptreact", "javascriptreact" }
 start_lsp("emmet_language_server", { "html", "css" })
 start_lsp("jsonls", { "json" })
 start_lsp("pyright", { "python" })
-start_lsp("omnisharp", { "cs" })
+
+start_lsp("omnisharp", { "cs" }, {
+  cmd = {
+    mason_bin("omnisharp") or "omnisharp",
+    "--languageserver",
+    "--hostPID",
+    tostring(vim.fn.getpid()),
+  },
+  root_dir = require("lspconfig.util").root_pattern("*.sln", "*.csproj", ".git"),
+  enable_editorconfig_support = true,
+  enable_ms_build_load_projects_on_demand = false,
+  enable_roslyn_analyzers = true,
+  organize_imports_on_format = true,
+  enable_import_completion = true,
+  sdk_include_prereleases = true,
+  analyze_open_documents_only = false,
+  handlers = {
+    ["textDocument/definition"] = require("omnisharp_extended").handler,
+  },
+  capabilities = vim.tbl_deep_extend("force", capabilities, {
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = true,
+          preselectSupport = true,
+          insertReplaceSupport = true,
+          labelDetailsSupport = true,
+          deprecatedSupport = true,
+          commitCharactersSupport = true,
+          tagSupport = { valueSet = { 1 } },
+          resolveSupport = {
+            properties = {
+              "documentation",
+              "detail",
+              "additionalTextEdits",
+            },
+          },
+        },
+      },
+    },
+  }),
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = true
+    local buf = vim.lsp.buf
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+
+    vim.keymap.set("n", "gd", buf.definition, opts)
+    vim.keymap.set("n", "gr", buf.references, opts)
+    vim.keymap.set("n", "K", buf.hover, opts)
+    vim.keymap.set("n", "<leader>rn", buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", buf.code_action, opts)
+    vim.keymap.set("n", "<leader>f", function() buf.format({ async = true }) end, opts)
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function() buf.format({ async = false }) end,
+    })
+  end,
+})
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "lua",
   callback = function()
     vim.lsp.start({
       name = "lua_ls",
-      cmd = get_server_cmd("lua_ls"),
+      cmd = { mason_bin("lua-language-server") or "lua-language-server" },
       root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1]),
       settings = {
         Lua = {
@@ -123,8 +171,8 @@ if rt_ok then
     vim.keymap.set("n", "<leader>rd", rust_tools.debuggables.debuggables, opts)
   end
 
-  vim.lsp.config('rust_analyzer', {
-    cmd = get_server_cmd("rust_analyzer"),
+  vim.lsp.config("rust_analyzer", {
+    cmd = { mason_bin("rust-analyzer") or "rust-analyzer" },
     settings = {
       ["rust-analyzer"] = {
         cargo = { allFeatures = true },
@@ -141,7 +189,7 @@ if rt_ok then
       local root_dir = vim.fs.dirname(vim.fs.find({ "Cargo.toml", ".git" }, { upward = true })[1])
       vim.lsp.start({
         name = "rust_analyzer",
-        cmd = get_server_cmd("rust_analyzer"),
+        cmd = { mason_bin("rust-analyzer") or "rust-analyzer" },
         root_dir = root_dir,
         settings = {
           ["rust-analyzer"] = {
