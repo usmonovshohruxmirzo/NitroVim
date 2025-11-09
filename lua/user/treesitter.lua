b9531a8fb78eb06fc5a5d4b4b42d7670c248dc95
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2025-11-09 21:29:01 +0500

    fix: replace semicolon with comma in nvim-treesitter config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -15,7 +15,7 @@ require("nvim-treesitter.configs").setup({
   },
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = false;
+    additional_vim_regex_highlighting = false,
   },
   indent = { enable = true },
 })
