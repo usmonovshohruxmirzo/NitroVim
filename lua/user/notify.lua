14141c735a98e6fe284b00326999e33990ceee0f
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2025-11-16 18:12:38 +0500

    fix(noice): show actual macro text when recording @<register>

--- lua/user/notify.lua
+++ lua/user/notify.lua
@@ -1,3 +1,19 @@
+---@diagnostic disable: undefined-global
+
 require("notify").setup({
   background_colour = "#000000",
+})
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function(args)
+    local reg = args.data and args.data.register or "a"
+    vim.notify("Recording @" .. reg, "info", { timeout = 2000 })
+  end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function(args)
+    local reg = args.data and args.data.register or "a"
+    vim.notify("Recording @" .. reg .. " stopped", "info", { timeout = 2000 })
+  end,
 })
