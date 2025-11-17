b1be6adc59ffc6ce03fc0cab8e71f5d4e0e42aa6
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2025-11-17 14:02:54 +0500

    fix: Redefined local `opts`

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -1,5 +1,5 @@
 --- @diagnostic disable: undefined-global
---- @type table
+---@diagnostic disable: redefined-local
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
