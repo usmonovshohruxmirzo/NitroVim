6ac428170566bf64647eae4d67f75faacc8b566f
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-17 12:08:33 +0500

    refactor(options): remove vim diagnostics

--- lua/nitro/core/options.lua
+++ lua/nitro/core/options.lua
@@ -44,33 +44,3 @@ vim.api.nvim_create_autocmd("FileChangedShell", {
     end
   end,
 })
-
-vim.diagnostic.config({
-  virtual_text = { prefix = "●", spacing = 2 },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = " ",
-      [vim.diagnostic.severity.WARN]  = " ",
-      [vim.diagnostic.severity.HINT]  = " 󰋗",
-      [vim.diagnostic.severity.INFO]  = " ",
-    },
-    numhl = {
-      [vim.diagnostic.severity.ERROR] = "",
-      [vim.diagnostic.severity.WARN]  = "",
-      [vim.diagnostic.severity.HINT]  = "",
-      [vim.diagnostic.severity.INFO]  = "",
-    },
-  },
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
-  float = {
-    border = "rounded",
-    focusable = true,
-    max_width = 80,
-    wrap = true,
-    source = "always",
-    header = "",
-    prefix = "",
-  },
-})
